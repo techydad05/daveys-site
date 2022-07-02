f9710ba494405d279b7cd18350aedfe1b81b99bf
Ivan <techydad05@gmail.com>
2022-07-01 23:10:00 -0400

    adding a few things

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="container">
+    <img src="/daveys-logo-2.png" alt="" width="80%">
+</div>
+
+<style>
+    .container {
+        height: 100vh;
+        width: 100vw;
+        background: black;
+        display: flex;
+        position: fixed;
+        align-items: center;
+        justify-content: center;
+        margin: 0 !important;
+        padding: 0 !important;
+    }
+    body {
+        background: black;
+    }
+</style>
